9f0f67a18083b818ba1ca09502a4f74b14be52af
mdakashsheikh <nmakashsheikh@gmail.com>
2025-01-08 00:24:03 +0600

    learning exac

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -14,7 +14,7 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prismaService.product.findMany();
   }
 
   findOne(id: number) {
